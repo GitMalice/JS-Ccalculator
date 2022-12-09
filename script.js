9ce922e413752f0db4ce862af3b216aee3d441f8
Malice <create@hagalaz.fr>
2022-12-09 21:19:01 +0100

    implemented the storage of numbers and operations

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+let display_value = "";
+let screen = document.querySelector(".screen");
+let numbers = [];
+let operationsList = [];
+
+let key_1 = document.querySelector(".numbers ul li:nth-of-type(1)");
+let key_2 = document.querySelector(".numbers ul li:nth-of-type(2)");
+let key_3 = document.querySelector(".numbers ul li:nth-of-type(3)");
+let key_4 = document.querySelector(".numbers ul li:nth-of-type(4)");
+let key_5 = document.querySelector(".numbers ul li:nth-of-type(5)");
+let key_6 = document.querySelector(".numbers ul li:nth-of-type(6)");
+let key_7 = document.querySelector(".numbers ul li:nth-of-type(7)");
+let key_8 = document.querySelector(".numbers ul li:nth-of-type(8)");
+let key_9 = document.querySelector(".numbers ul li:nth-of-type(9)");
+
+let plus = document.querySelector(".operators ul li:nth-of-type(1)");
+let minus = document.querySelector(".operators ul li:nth-of-type(2)");
+let per = document.querySelector(".operators ul li:nth-of-type(3)");
+let divided = document.querySelector(".operators ul li:nth-of-type(4)");
+
+let clear = document.querySelector(".clear");
+
+// function createNumbersVariables() {
+//   let key_number = [];
+//   for (let i = 0; i < 9; i++) {
+//     key_number[i] = i;
+//   }
+//   return key_number;
+// }
+
 function add(operandA, operandB) {
   console.log(operandA + operandB);
   return operandA + operandB;
@@ -37,4 +67,82 @@ function operate(operandA, operandB, operation) {
   }
 }
 
-console.log(operate(3, 6, "divide"));
+key_1.addEventListener("click", () => {
+  display_value += "1";
+  screen.innerText = display_value;
+});
+key_2.addEventListener("click", () => {
+  display_value += "2";
+  screen.innerText = display_value;
+});
+key_3.addEventListener("click", () => {
+  display_value += "3";
+  screen.innerText = display_value;
+});
+key_4.addEventListener("click", () => {
+  display_value += "4";
+  screen.innerText = display_value;
+});
+key_5.addEventListener("click", () => {
+  display_value += "5";
+  screen.innerText = display_value;
+});
+key_6.addEventListener("click", () => {
+  display_value += "6";
+  screen.innerText = display_value;
+});
+key_7.addEventListener("click", () => {
+  display_value += "7";
+  screen.innerText = display_value;
+});
+key_8.addEventListener("click", () => {
+  display_value += "8";
+  screen.innerText = display_value;
+});
+key_9.addEventListener("click", () => {
+  display_value += "9";
+  screen.innerText = display_value;
+});
+
+clear.addEventListener("click", () => {
+  display_value = "";
+  screen.innerText = display_value;
+});
+
+function store(value) {
+  numbers.push(value);
+  display_value = "";
+  screen.innerText = display_value;
+  console.log(numbers);
+  console.log(operationsList);
+}
+
+plus.addEventListener("click", () => {
+  if (display_value !== "") {
+    store(display_value);
+    operationsList.push("add");
+  }
+});
+
+minus.addEventListener("click", () => {
+  if (display_value !== "") {
+    store(display_value);
+    operationsList.push("substract");
+  }
+});
+
+per.addEventListener("click", () => {
+  if (display_value !== "") {
+    store(display_value);
+    operationsList.push("multiply");
+  }
+});
+
+divided.addEventListener("click", () => {
+  if (display_value !== "") {
+    store(display_value);
+    operationsList.push("divide");
+  }
+});
+
+screen.innerText = display_value;
