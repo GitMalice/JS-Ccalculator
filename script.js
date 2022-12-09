206445af8ce0aedfdcf45b59c70387fd39bc8e7a
Malice <create@hagalaz.fr>
2022-12-09 23:19:30 +0100

    made the calculator work my own way

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let screen = document.querySelector(".screen");
 let numbers = [];
 let operationsList = [];
 let result;
+let tempResult;
 
 let key_1 = document.querySelector(".numbers ul li:nth-of-type(1)");
 let key_2 = document.querySelector(".numbers ul li:nth-of-type(2)");
@@ -13,6 +14,7 @@ let key_6 = document.querySelector(".numbers ul li:nth-of-type(6)");
 let key_7 = document.querySelector(".numbers ul li:nth-of-type(7)");
 let key_8 = document.querySelector(".numbers ul li:nth-of-type(8)");
 let key_9 = document.querySelector(".numbers ul li:nth-of-type(9)");
+let key_0 = document.querySelector(".numbers ul li:nth-of-type(10)");
 
 let plus = document.querySelector(".operators ul li:nth-of-type(1)");
 let minus = document.querySelector(".operators ul li:nth-of-type(2)");
@@ -22,13 +24,9 @@ let divided = document.querySelector(".operators ul li:nth-of-type(4)");
 let clear = document.querySelector(".clear");
 let equals = document.querySelector(".equals");
 
-// function createNumbersVariables() {
-//   let key_number = [];
-//   for (let i = 0; i < 9; i++) {
-//     key_number[i] = i;
-//   }
-//   return key_number;
-// }
+let numbersSelect = document.querySelector(".numbers");
+
+let resultDisplayed;
 
 function add(operandA, operandB) {
   console.log(operandA + operandB);
@@ -70,39 +68,53 @@ function operate(operandA, operandB, operation) {
 }
 
 key_1.addEventListener("click", () => {
+  check();
   display_value += "1";
   screen.innerText = display_value;
 });
 key_2.addEventListener("click", () => {
+  check();
   display_value += "2";
   screen.innerText = display_value;
 });
 key_3.addEventListener("click", () => {
+  check();
   display_value += "3";
   screen.innerText = display_value;
 });
 key_4.addEventListener("click", () => {
+  check();
   display_value += "4";
   screen.innerText = display_value;
 });
 key_5.addEventListener("click", () => {
+  check();
   display_value += "5";
   screen.innerText = display_value;
 });
 key_6.addEventListener("click", () => {
+  check();
   display_value += "6";
   screen.innerText = display_value;
 });
 key_7.addEventListener("click", () => {
+  check();
   display_value += "7";
   screen.innerText = display_value;
 });
 key_8.addEventListener("click", () => {
+  check();
   display_value += "8";
   screen.innerText = display_value;
 });
 key_9.addEventListener("click", () => {
+  check();
   display_value += "9";
+  screen.innerText = display_value;
+});
+key_0.addEventListener("click", () => {
+  check();
+  display_value += "0";
   screen.innerText = display_value;
 });
 
@@ -117,13 +129,22 @@ function store(value) {
   numbers.push(value);
   display_value = "";
   screen.innerText = display_value;
-  // console.log(numbers);
+}
+
+function check() {
+  if (resultDisplayed) {
+    resultDisplayed = false;
+    display_value = "";
+    screen.innerText = display_value;
+  }
 }
 
 plus.addEventListener("click", () => {
   if (display_value !== "") {
     store(display_value);
     operationsList.push("add");
+    console.log(operationsList);
+    console.log(numbers);
   }
 });
 
@@ -150,30 +171,27 @@ divided.addEventListener("click", () => {
 
 equals.addEventListener("click", () => {
   store(display_value);
-  // console.log("result:");
-  // console.log(numbers);
-  // console.log(operationsList);
-
   result = parseInt(numbers[0]);
-
-  for (let a = 0; a < operationsList.length; a++) {
-    result = operate(result, parseInt(numbers[a + 1]), operationsList[a]);
-    // console.log(result);
+  if (numbers[numbers.length - 1] == "") {
+    console.log("woopsie");
+    for (let a = 0; a < operationsList.length - 1; a++) {
+      result = operate(result, parseInt(numbers[a + 1]), operationsList[a]);
+    }
+  } else {
+    for (let a = 0; a < operationsList.length; a++) {
+      result = operate(result, parseInt(numbers[a + 1]), operationsList[a]);
+    }
   }
+
+  console.log(result.toString().length);
+  if (result.toString().length > 12) {
+    result = result.toPrecision(12);
+  }
+
   display_value = result;
   screen.innerText = display_value;
 
   numbers = [];
   operationsList = [];
-
-  // tempResult = numbers[0];
-  // for (let a = 0; a < operationsList.length; a++) {
-  //   tempResult = operate(tempResult, numbers[a + 1], operationsList[a]);
-  //   return tempResult;
-  // }
-  // display_value = tempResult;
-  // screen.innerText = display_value;
-  // console.log(tempResult);
+  resultDisplayed = true;
 });
-
-// screen.innerText = display_value;
