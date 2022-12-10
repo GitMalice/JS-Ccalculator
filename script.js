0a6c34e184135a389b57be895d8fec9ea48d4b7c
Malice <create@hagalaz.fr>
2022-12-10 01:13:44 +0100

    added the keyboard recognition

--- script.js
+++ script.js
@@ -105,65 +105,74 @@ function calculate() {
   screen.innerText = display_value;
 }
 
-key_1.addEventListener("click", () => {
+function event1() {
   check();
   display_value += "1";
   screen.innerText = display_value;
-});
-key_2.addEventListener("click", () => {
+}
+
+function event2() {
   check();
   display_value += "2";
   screen.innerText = display_value;
-});
-key_3.addEventListener("click", () => {
+}
+
+function event3() {
   check();
   display_value += "3";
   screen.innerText = display_value;
-});
-key_4.addEventListener("click", () => {
+}
+
+function event4() {
   check();
   display_value += "4";
   screen.innerText = display_value;
-});
-key_5.addEventListener("click", () => {
+}
+
+function event5() {
   check();
   display_value += "5";
   screen.innerText = display_value;
-});
-key_6.addEventListener("click", () => {
+}
+
+function event6() {
   check();
   display_value += "6";
   screen.innerText = display_value;
-});
-key_7.addEventListener("click", () => {
+}
+
+function event7() {
   check();
   display_value += "7";
   screen.innerText = display_value;
-});
-key_8.addEventListener("click", () => {
+}
+
+function event8() {
   check();
   display_value += "8";
   screen.innerText = display_value;
-});
-key_9.addEventListener("click", () => {
+}
+
+function event9() {
   check();
   display_value += "9";
   screen.innerText = display_value;
-});
-key_0.addEventListener("click", () => {
+}
+
+function event0() {
   check();
   display_value += "0";
   screen.innerText = display_value;
-});
+}
 
-clear.addEventListener("click", () => {
+function eventClear() {
   display_value = "";
   screen.innerText = display_value;
   numbers = [];
   operationsList = [];
-});
+}
 
-plus.addEventListener("click", () => {
+function eventAdd() {
   if (display_value !== "") {
     store(display_value);
     operationsList.push("add");
@@ -173,49 +182,129 @@ plus.addEventListener("click", () => {
   //   calculate();
   //   resultDisplayed = true;
   // }
-});
+}
 
-minus.addEventListener("click", () => {
+function eventSubstract() {
   if (display_value !== "") {
     store(display_value);
     operationsList.push("substract");
   }
-});
+}
 
-per.addEventListener("click", () => {
+function eventMultiply() {
   if (display_value !== "") {
     store(display_value);
     operationsList.push("multiply");
   }
-});
+}
 
-divided.addEventListener("click", () => {
+function eventDivide() {
   if (display_value !== "") {
     store(display_value);
     operationsList.push("divide");
   }
-});
+}
 
-point.addEventListener("click", () => {
+function eventPoint() {
   if (display_value != "" && !resultDisplayed) {
     if (!display_value.includes(".")) {
       display_value += ".";
       screen.innerText = display_value;
     }
   }
-});
+}
 
-backspace.addEventListener("click", () => {
+function eventBackspace() {
   display_value = display_value.toString().slice(0, -1);
   console.log(display_value);
   screen.innerText = display_value;
-});
+}
 
-equals.addEventListener("click", () => {
+function eventEquals() {
   store(display_value);
   calculate();
 
   numbers = [];
   operationsList = [];
   resultDisplayed = true;
-});
+}
+
+key_1.addEventListener("click", event1);
+key_2.addEventListener("click", event2);
+key_3.addEventListener("click", event3);
+key_4.addEventListener("click", event4);
+key_5.addEventListener("click", event5);
+key_6.addEventListener("click", event6);
+key_7.addEventListener("click", event7);
+key_8.addEventListener("click", event8);
+key_9.addEventListener("click", event9);
+key_0.addEventListener("click", event0);
+
+clear.addEventListener("click", eventClear);
+plus.addEventListener("click", eventAdd);
+minus.addEventListener("click", eventSubstract);
+per.addEventListener("click", eventMultiply);
+divided.addEventListener("click", eventDivide);
+point.addEventListener("click", eventPoint);
+backspace.addEventListener("click", eventBackspace);
+equals.addEventListener("click", eventEquals);
+
+document.body.addEventListener("keypress", updateKey);
+
+function updateKey(e) {
+  //console.log(e);
+  if (e.key == "Enter") {
+    eventEquals();
+  }
+  if (e.key == "+") {
+    eventAdd();
+  }
+  if (e.key == "-") {
+    eventSubstract();
+  }
+  if (e.key == "*") {
+    eventMultiply();
+  }
+  if (e.key == "/") {
+    eventDivide();
+  }
+  if (e.key == "d") {
+    eventBackspace();
+  }
+  if (e.key == "a") {
+    eventClear();
+  }
+  if (e.key == ".") {
+    eventPoint();
+  }
+  if (e.key == "0") {
+    event0();
+  }
+  if (e.key == "1") {
+    event1();
+  }
+  if (e.key == "2") {
+    event2();
+  }
+  if (e.key == "3") {
+    event3();
+  }
+  if (e.key == "4") {
+    event4();
+  }
+  if (e.key == "5") {
+    event5();
+  }
+  if (e.key == "6") {
+    event6();
+  }
+  if (e.key == "7") {
+    event7();
+  }
+  if (e.key == "8") {
+    event8();
+  }
+  if (e.key == "9") {
+    event9();
+  }
+}
